2fce7af7ded052137ea2cf0f576d63cccc600c8b
Nyasha Chizampeni <nchizampeni@gmail.com>
2024-08-17 13:20:25 +0200

    save

---
+++ stegnography/forms.py
@@ -0,0 +1,6 @@
+# myapp/forms.py
+from django import forms
+
+class EncodeForm(forms.Form):
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Message to hide'}))
+    cover_image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control'}))
